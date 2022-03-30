719be3fa86c62d256b0b2140d7632c3ddeb9b426
Ilya Schmitt <mr.schmitt@hotmail.de>
2022-03-31 01:06:09 +0200

    final touch

--- src/script.js
+++ src/script.js
@@ -2,14 +2,13 @@ import './style.css'
 import * as THREE from 'three'
 import * as dat from 'lil-gui'
 import gsap from 'gsap'
-
 /**
  * Debug
  */
 
 window.onload = ()=>{
 
-    if(window.innerWidth >= 1768){
+    if(window.innerWidth >= 600){
 
 
     const parameters = {
@@ -30,8 +29,8 @@ window.onload = ()=>{
      */
     // Texture
     const textureLoader = new THREE.TextureLoader()
-    const gradientTexture = textureLoader.load('')
-    gradientTexture.magFilter = THREE.NearestFilter
+    const starTexture = textureLoader.load('/images/singlestar.png')
+
 
     // Material
     const material = new THREE.MeshToonMaterial({
@@ -49,17 +48,28 @@ window.onload = ()=>{
         material
     )
     const mesh3 = new THREE.Mesh(
-        new THREE.BoxBufferGeometry(0.25, 0.25, 0.25, 16),
+        new THREE.BoxBufferGeometry(0.35, 0.35, 0.35, 16),
         material
     )
+    if(window.innerWidth >= 1200){   
+        mesh1.position.x = -2.6
+        mesh2.position.x = 2.6
+        mesh3.position.x = -2.6
 
-    mesh1.position.x = - 2.5
-    mesh2.position.x = 2.5
-    mesh3.position.x = -2.5
+        mesh1.position.y =  objectsDistance * 0.3
+        mesh2.position.y = - objectsDistance * 0.7
+        mesh3.position.y = - objectsDistance * 1.7
+        
+    }else{
+        mesh1.position.x = 0
+        mesh2.position.x = 1.2
+        mesh3.position.x = -1.2
 
-    mesh1.position.y =  objectsDistance * 0.3
-    mesh2.position.y = - objectsDistance * 0.7
-    mesh3.position.y = - objectsDistance * 1.7
+        mesh1.position.y = - objectsDistance * 0.7
+        mesh2.position.y = - objectsDistance * 0.7
+        mesh3.position.y = - objectsDistance * 0.7
+    }
+
 
     scene.add(mesh1, mesh2, mesh3)
 
@@ -76,7 +86,7 @@ window.onload = ()=>{
      * Particles
      */
     // Geometry
-    const particlesCount = 200
+    const particlesCount = 500
     const positions = new Float32Array(particlesCount * 3)
 
     for(let i = 0; i < particlesCount; i++)
@@ -92,13 +102,49 @@ window.onload = ()=>{
     // Material
     const particlesMaterial = new THREE.PointsMaterial({
         color: parameters.materialColor,
+        map: starTexture,
         sizeAttenuation: textureLoader,
-        size: 0.03
+        size: 0.1,
+        transparent: true
     })
 
     // Points
     const particles = new THREE.Points(particlesGeometry, particlesMaterial)
     scene.add(particles)
+
+
+      // Raindrops
+            
+      const rainGeometry = new THREE.BufferGeometry()
+      const count = 500
+      
+      const vertices = new Float32Array(count * 3)
+      
+      for(let i = 0; i < count * 3; i++ ){
+        
+        vertices[i] = (Math.random()-0.5)*5
+      }
+      
+      rainGeometry.setAttribute(
+        'position',
+        new THREE.BufferAttribute(vertices,3)
+        )
+        
+        const p = rainGeometry.attributes.position.array
+        
+        const rainMaterial = new THREE.PointsMaterial({
+            size: 0.013,
+            sizeAttenuation: true,
+            transparent: true,
+            color: new THREE.Color(0xffffff),
+            depthTest: false,
+            depthWrite: false
+        })
+        
+        const rain = new THREE.Points(rainGeometry, rainMaterial)
+        rain.position.y = -objectsDistance 
+        scene.add(rain)
+
 
     /**
      * Sizes
@@ -213,6 +259,14 @@ window.onload = ()=>{
             mesh.rotation.y += deltaTime * 0.12
         }
 
+        for ( let i = 1; i <= (count * 3); i = i+3 ) {
+            p[i] =p[i] - 0.03
+            if(p[i] < -2){
+              p[i] = 2.5
+            }
+          }
+          rainGeometry.attributes.position.needsUpdate = true;
+          
         // Render
         renderer.render(scene, camera)
 
@@ -236,8 +290,8 @@ window.onload = ()=>{
         }
         const time = today.getHours() + ":" + minutes
         const month = today.getMonth()
-        const weekday = today.getDay() % 7
-        let day = today.getDay()
+        const weekday = today.getDay()
+        let day = today.getDate()
         if(day < 10){
         day = '0'+ day
         }
@@ -250,24 +304,127 @@ window.onload = ()=>{
     function navbarShow(){
         const navbar = document.getElementById('navbar')
          const body = document.getElementsByTagName('body')[0]
-         document.addEventListener('scroll', () => {
-           if(scrollY > window.innerHeight){
+         if(scrollY > window.innerHeight){
              navbar.style.display = 'inline'
-           }else{
-             navbar.style.display = 'none'
-           }
+            }else{
+                navbar.style.display = 'none'
+            }
+        }
+        
+        const img1 = document.getElementById('img-1')
+        function imgScrollTop(){
+            if(window.innerWidth<600){
+                let scrollValue = -0.25 * window.scrollY
+                img1.style.transform = 'translateY('+scrollValue+'px)'
+            }
+        }
+        
+        document.addEventListener('scroll', () => {
+            
+            imgScrollTop()
+            navbarShow()
+                
         })
-      }
-      
-      navbarShow()
-      
-      
-      function scrollUp(){
-         document.getElementById('up-button').addEventListener('click', () => {
-           window.scrollTo({
-             top: 0, behavior: 'smooth'
-           })
-        })  
-      }
-      scrollUp()
+
+    const img2 = document.getElementById('img-2')
+    const work = document.getElementById('work-button')
+    const resume = document.getElementById('resume-button')
+    const contactMe = document.getElementById('contact-me')
+    const contact = document.getElementById('contact-button')
+    
+    function imgHover(){
+        if(window.innerWidth > 992){
+            img1.addEventListener('mouseover', () => {
+                work.classList.add('shake')
+            })
+            img1.addEventListener('mouseout',() => {
+                work.classList.remove('shake')
+            })
+            img2.addEventListener('mouseover', () => {
+                resume.classList.add('shake')
+            })
+            img2.addEventListener('mouseout',() => {
+                resume.classList.remove('shake')
+            })
+            contactMe.addEventListener('mouseover', () => {
+                contact.classList.add('shake')
+            })
+            contactMe.addEventListener('mouseout',() => {
+                contact.classList.remove('shake')
+            })
+        }
+    }
+    imgHover()
+
+    const container = document.getElementsByClassName('card-container');
+    const inner = document.getElementsByClassName('card-inner');
+    
+    for(let i=0;i<container.length;i++){
+
+        container[i].addEventListener('mouseover', (e) =>{
+
+            
+            const onMouseEnterHandler = function(event) {
+                update(event);
+            };
+            const onMouseLeaveHandler = function() {
+                inner[i].style = "";
+                inner[i].style.transition = "transform 1000ms cubic-bezier(0.68, 0.5, 0.3, 8)";
+            };
+            const onMouseMoveHandler = function(event) {
+                if (isTimeToUpdate()) {
+                    update(event);
+                }
+            };
+            
+            
+            
+            container[i].onmouseenter = onMouseEnterHandler;
+            container[i].onmouseleave = onMouseLeaveHandler;
+            container[i].onmousemove = onMouseMoveHandler;
+            
+            let counter = 0;
+            const updateRate = 10;
+            const isTimeToUpdate = function() {
+                return counter++ % updateRate === 0;
+            };
+            
+            // Mouse 
+            var mouse = {
+                _x: 0,
+                _y: 0,
+                x: 0,
+                y: 0,
+                updatePosition: function(event) {
+                    const e = event || window.event;
+                    this.x = e.clientX - this._x;
+                    this.y = (e.clientY - this._y) * -1;
+                },
+                setOrigin: function(e) {
+                    this._x = e.offsetLeft + Math.floor(e.offsetWidth/2);
+                    this._y = e.offsetTop + Math.floor(e.offsetHeight/2);
+                },
+                show: function() { return '(' + this.x + ', ' + this.y + ')'; }
+            }
+            
+            mouse.setOrigin(container[i]);
+            
+            
+            const update = function(event) {
+                mouse.updatePosition(event);
+        updateTransformStyle(
+            (mouse.y / inner[i].offsetHeight/2).toFixed(2),
+            (mouse.x / inner[i].offsetWidth/2).toFixed(2)
+            );
+        };
+        
+        const updateTransformStyle = function(x, y) {
+            const style = "rotateX(" + x + "deg) rotateY(" + y + "deg)";
+            inner[i].style.transform = style;
+            inner[i].style.mozTransform = style;
+            inner[i].style.msTransform = style;
+            inner[i].style.oTransform = style;
+        }
+    })
+    }
 }
